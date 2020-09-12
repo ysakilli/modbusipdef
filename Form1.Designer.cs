6e3397b1fe783c321c1d683aa812be7feb8091d5
Yusuf Semih Akıllı <48387812+ysakilli@users.noreply.github.com>
2020-09-12 13:22:19 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Modbusp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.IPadressinput = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 63);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Disconnect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 158);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(216, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 231);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(90, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "IP Adress";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(130, 366);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(98, 22);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(234, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(363, 376);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // IPadressinput
+            // 
+            this.IPadressinput.Culture = new System.Globalization.CultureInfo("ts-ZA");
+            this.IPadressinput.Location = new System.Drawing.Point(12, 258);
+            this.IPadressinput.Mask = "000.000.000.000";
+            this.IPadressinput.Name = "IPadressinput";
+            this.IPadressinput.Size = new System.Drawing.Size(101, 22);
+            this.IPadressinput.TabIndex = 6;
+            this.IPadressinput.Text = "1234444";
+            this.IPadressinput.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.IPadressinput_MaskInputRejected);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 400);
+            this.Controls.Add(this.IPadressinput);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.MaskedTextBox IPadressinput;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+
+
+
+namespace Modbusp
+{
+    public partial class Form1 : Form
+    {
+        string m;
+
+        ModbusClient modbusClient = new ModbusClient("172.27.0.100 ", 502);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m = IPadressinput.Text;
+
+            ModbusClient modbusClient = new ModbusClient( m, 502);
+
+                       
+
+            if (modbusClient.Connected)
+            {
+
+                modbusClient.Disconnect();
+
+            }
+
+            
+
+            modbusClient.Connect();
+                   
+            Connectioncontroller();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            modbusClient.Disconnect();
+            Connectioncontroller();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Checkcontroller();
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            modbusClient.IPAddress = m;
+           
+        }
+
+        
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = modbusClient.IPAddress;
+            
+        }
+
+        private void Connectioncontroller()
+        {
+            
+                if (modbusClient.Connected)
+            {
+                textBox1.Text = "Connected";
+                MessageBox.Show("Connected");
+            }
+            else
+            {
+                textBox1.Text = "Disconnected";
+                MessageBox.Show("Disconnected");
+
+            }
+
+        }
+
+        private void Checkcontroller()
+        {
+            IPadressinput.Enabled = checkBox1.Checked;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void IPadressinput_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+    }
+}
